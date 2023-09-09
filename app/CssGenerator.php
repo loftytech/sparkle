85de87767e406ea796369f3c7ba1a22dd2b36ec9
loftytech <58659264+loftytech@users.noreply.github.com>
2023-09-09 23:56:38 +0100

    added support for media queries

--- app/CssGenerator.php
+++ app/CssGenerator.php
@@ -7,8 +7,11 @@ class CssGenerator {
     public string $primary_css = "";
     public string $nested_css = "";
     public string $compiled_css = "";
+    public string $compiled_media_queries = "";
 
     public array $compiled_array = [];
+    public array $temp_arr = [];
+    public array $media_query_arr = [];
 
     public function __construct($scss) {
         $json_string = $this->scssToNestedArray($scss);
@@ -21,7 +24,7 @@ class CssGenerator {
         $this->generateCss();
     }
 
-    public function scssToNestedArray($scss) {
+    private function scssToNestedArray($scss) {
         // convert selectors to objects
         $processedContent = preg_replace_callback("/(.*)(\{)/", function ($matches) {
             return '"'.trim($matches[1]).'":'.'{';
@@ -44,10 +47,96 @@ class CssGenerator {
         return "{".$processedContent."}";
     }
 
-    public function nestedArraysTocss($nestedArray, $parent_selector = "") {
+    private function nestedArraysTocss($nestedArray, $parent_selector = "") {
         foreach ($nestedArray as $key => $value) {
 
+            if (trim($key)[0] == "@") {
+
+                $this->extractMediaQueries($value, $parent_selector, $key);
+            } else {
+                if (gettype($value) == "string") {
+                
+                    $full_selector = $parent_selector;
+                    if ($parent_selector == "") {
+                        $full_selector = $key;
+                    }
+    
+                    if (trim($key)[0] == ":") {
+                        $full_selector = trim($parent_selector) . trim($key);
+                    }
+    
+                    if (!array_key_exists($full_selector, $this->compiled_array)) {
+                        $this->compiled_array[$full_selector] = ["$key: $value;"];
+                    } else {
+                        $style_arr = $this->compiled_array[$full_selector];
+                        array_push($style_arr, "$key: $value;");
+    
+                        $this->compiled_array[$full_selector] = $style_arr;
+                    }
+                } else {
+    
+                    foreach ($value as $child_key => $child_value) {
+                        if (gettype($child_value) == "string") {
+                            $full_selector = $parent_selector . " ". $key;
+    
+                            if ($parent_selector == "") {
+                                $full_selector = $key;
+                            }
+    
+                            if (trim($key)[0] == ":") {
+                                $full_selector = trim($parent_selector) . trim($key);
+                            }
+                            
+                            if (!array_key_exists($full_selector, $this->compiled_array)) {
+                                $this->compiled_array[$full_selector] = ["$child_key: $child_value;"];
+                            } else {
+                                $style_arr = $this->compiled_array[$full_selector];
+                                array_push($style_arr, "$child_key: $child_value;");
+    
+                                $this->compiled_array[$full_selector] = $style_arr;
+                            }
+                        } else {
+    
+    
+    
+                            $trim_key = trim($key);
+                            $trim_child_key = trim($child_key);
+                            $trim_parent_selector = trim($parent_selector);
+    
+                            $full_selector = $trim_parent_selector . " " . $trim_key . " " . $trim_child_key;
+    
+    
+                            if ($parent_selector == "") {
+                                $full_selector =  $trim_key . " " . $trim_child_key;
+                            }
+    
+                            $full_selector = $trim_parent_selector . ($trim_key[0] == ":" ? $trim_key : " " . $trim_key) . ($trim_child_key[0] == ":" ? $trim_child_key : " " . $trim_child_key);
+                            
+    
+                            if ($trim_child_key[0] == "@") {
+                                $this->extractMediaQueries($value->$child_key, $trim_parent_selector . ($trim_key[0] == ":" ? $trim_key : " " . $trim_key), $trim_child_key);
+                            } else {
+                                $this->nestedArraysTocss($value->$child_key, $full_selector);
+                            }
+                        }
+                    }
+                }
+            }
             
+        }
+    }
+
+    private function extractMediaQueries($nestedArray, $parent_selector, $scope_selector = "") {
+        $this->temp_arr = [];
+
+        $this->resolveCss($nestedArray, $parent_selector);
+
+        array_push($this->media_query_arr, [$scope_selector => $this->temp_arr]);
+    }
+
+
+    private function resolveCss($nestedArray, $parent_selector) {
+        foreach ($nestedArray as $key => $value) {
             if (gettype($value) == "string") {
                 $full_selector = $parent_selector;
                 if ($parent_selector == "") {
@@ -58,13 +147,13 @@ class CssGenerator {
                     $full_selector = trim($parent_selector) . trim($key);
                 }
 
-                if (!array_key_exists($full_selector, $this->compiled_array)) {
-                    $this->compiled_array[$full_selector] = ["$key: $value;"];
+                if (!array_key_exists($full_selector, $this->temp_arr)) {
+                    $this->temp_arr[$full_selector] = ["$key: $value;"];
                 } else {
-                    $style_arr = $this->compiled_array[$full_selector];
+                    $style_arr = $this->temp_arr[$full_selector];
                     array_push($style_arr, "$key: $value;");
 
-                    $this->compiled_array[$full_selector] = $style_arr;
+                    $this->temp_arr[$full_selector] = $style_arr;
                 }
             } else {
 
@@ -80,13 +169,13 @@ class CssGenerator {
                             $full_selector = trim($parent_selector) . trim($key);
                         }
                         
-                        if (!array_key_exists($full_selector, $this->compiled_array)) {
-                            $this->compiled_array[$full_selector] = ["$child_key: $child_value;"];
+                        if (!array_key_exists($full_selector, $this->temp_arr)) {
+                            $this->temp_arr[$full_selector] = ["$child_key: $child_value;"];
                         } else {
-                            $style_arr = $this->compiled_array[$full_selector];
+                            $style_arr = $this->temp_arr[$full_selector];
                             array_push($style_arr, "$child_key: $child_value;");
 
-                            $this->compiled_array[$full_selector] = $style_arr;
+                            $this->temp_arr[$full_selector] = $style_arr;
                         }
                     } else {
 
@@ -102,16 +191,16 @@ class CssGenerator {
                         }
 
                         $full_selector = $trim_parent_selector . ($trim_key[0] == ":" ? $trim_key : " " . $trim_key) . ($trim_child_key[0] == ":" ? $trim_child_key : " " . $trim_child_key);
-                        
 
-                        $this->nestedArraysTocss($value->$child_key, $full_selector);
+                        $this->resolveCss($value->$child_key, $full_selector);
                     }
                 }
             }
         }
     }
 
-    public function generateCss() {
+
+    private function generateCss() {
         foreach ($this->compiled_array as $key => $value) {
             $this->compiled_css = $this->compiled_css . $key . " {";
             foreach ($value as $property => $style) {
@@ -119,6 +208,22 @@ class CssGenerator {
             }
             $this->compiled_css = $this->compiled_css .  "}";
         }
+
+        foreach ($this->media_query_arr as $key => $value) {
+            foreach ($value as $media_key => $media_queries) {
+                $this->compiled_media_queries = $this->compiled_media_queries . $media_key . " {";
+                foreach ($media_queries as $styles_key => $styles_value) {
+                    $this->compiled_media_queries = $this->compiled_media_queries . $styles_key . " {";
+                    foreach ($styles_value as $key => $css_style) {
+                        $this->compiled_media_queries = $this->compiled_media_queries . $css_style;
+                    }
+                    $this->compiled_media_queries = $this->compiled_media_queries . "}";
+                }
+                $this->compiled_media_queries = $this->compiled_media_queries .  "}";
+            }
+        }
+
+        $this->compiled_css = $this->compiled_css . " ". $this->compiled_media_queries;
     }
 
     
