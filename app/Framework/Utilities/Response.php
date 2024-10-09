b4c41c3865091e0f5cb0e4cdfb2a1c7cc5076369
loftytech <58659264+loftytech@users.noreply.github.com>
2024-10-09 12:55:38 +0100

    added support for application/x-www-form-urlencoded in HttpRequest

--- app/Framework/Utilities/Response.php
+++ app/Framework/Utilities/Response.php
@@ -5,7 +5,7 @@ class Response {
     public static function send(array $data, int $statusCode = 200) {
         header('Content-Type: application/json');
         http_response_code($statusCode);
-        echo json_encode($data);
+        echo json_encode($data, JSON_NUMERIC_CHECK);
         exit;
     }
 }
